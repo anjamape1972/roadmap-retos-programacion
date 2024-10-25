4b95fb72bf8fbdd26510eb940907516ee66d1810
anjamape1972 <anjamape1972@protonmail.com>
2024-10-26 01:16:04 +0200

    Create 00_sintaxis_variables_tipos_datos.py

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/00_sintaxis_variables_tipos_datos.py
@@ -0,0 +1,22 @@
+'''
+EJERCICIO:
+ * - Crea un comentario en el código y coloca la URL del sitio web oficial del
+ *   lenguaje de programación que has seleccionado.
+ * - Representa las diferentes sintaxis que existen de crear comentarios
+ *   en el lenguaje (en una línea, varias...).
+ * - Crea una variable (y una constante si el lenguaje lo soporta).
+ * - Crea variables representando todos los tipos de datos primitivos
+ *   del lenguaje (cadenas de texto, enteros, booleanos...).
+ * - Imprime por terminal el texto: "¡Hola, [y el nombre de tu lenguaje]!"
+'''
+#Este es un comentario en el que va incluida la URL de Python: https://www.python.org
+'''
+Este es un comentario
+de varias
+líneas
+'''
+edad = 0 #Variable tipo int
+estatura = 1.65 #Variable tipo float
+nombre = "Antonio Javier" #Variable tipo string
+hombre = True #Variable tipo booleana
+print ("¡Hola, Python!")
